c4b05ac7c3a5fe483788f1ef8e0053ae43c82768
Derik Banning <deriklee451@gmail.com>
2022-12-21 15:41:10 -0700

    savesssss

--- supper-setter-capstone.client/src/pages/HomePage.vue
+++ supper-setter-capstone.client/src/pages/HomePage.vue
@@ -1,16 +1,19 @@
 <template>
+  <!-- NOTE Here is some welcome stuff....  -->
   <section class="row justify-content-center container-fluid">
     <div class="col-8">
       <h1 class="text-center fw-bold raleway">
         Welcome! To Supper Setter
       </h1>
-      <p class="raleway fs-5">Cut your time spent planning your meals and groceries. You can manage recipes,
-        ingredients, food categories,
-        meal types & grocery items with this clean, uncluttered interface Our Meals combines recipe management, meal
-        planning, groceries</p>
+      <p class="raleway fs-5">Cut your time spent planning your meals and shopping list's. You can manage your recipes,
+        ingredients, meal types & grocery items with this clean, uncluttered interface Our Meals combines recipe
+        management, meal
+        planning & grocery shopping</p>
     </div>
 
   </section>
+  <!-- NOTE adding recipe cards here...  -->
+
 </template>
 
 <script>
