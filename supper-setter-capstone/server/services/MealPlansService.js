b462abc5be03f9ab5a9f88ff704f508a6a9a3e8f
LiamSmith1992 <liamj.smith@hotmail.com>
2022-12-21 16:25:28 -0700

    meals service account controller back end

---
+++ supper-setter-capstone/server/services/MealPlansService.js
@@ -0,0 +1,18 @@
+
+import { dbContext } from "../db/DbContext"
+import { logger } from "../utils/Logger"
+
+
+
+class MealPlansService {
+
+
+  // async getAccountMealPlans(accountId) {
+  //   const res = await dbContext.
+  // }
+
+
+}
+
+
+export const mealPlansService = new MealPlansService()
