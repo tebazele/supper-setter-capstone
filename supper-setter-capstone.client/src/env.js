5e94747d62f7b9b1fbea5abaedc0715285b77a99
Jeanne Allen <tebazele@gmail.com>
2022-12-21 12:18:11 -0700

    env js

--- supper-setter-capstone.client/src/env.js
+++ supper-setter-capstone.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'jeanne-allen.us.auth0.com'
+export const clientId = 'sqlbSe3a1IkpDUog43u7KloBLXDFMzBT'
+export const audience = 'https://jeannecoursework.com'
