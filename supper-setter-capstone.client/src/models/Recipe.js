eae09dae2df797fd9ea18e4c65ebd2abec77680c
Jeanne Allen <tebazele@gmail.com>
2022-12-21 15:05:02 -0700

    recipe model

--- supper-setter-capstone.client/src/models/Recipe.js
+++ supper-setter-capstone.client/src/models/Recipe.js
@@ -4,7 +4,10 @@ export class Recipe {
         this.title = data.recipe.label
         this.dietLabels = data.recipe.dietLabels
         // diet labels is an array
-
+        this.ingredientLines = data.recipe.ingredientLines
+        // ingredientLines is an array of strings
+        this.ingredients = data.recipe.ingredients
+        // array of objects -- we care about ingredients[i].food
 
     }
 }
