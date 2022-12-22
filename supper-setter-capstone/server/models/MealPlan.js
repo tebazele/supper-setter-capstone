a414c9e73bbca682ecd28788052ed7d08f04c010
Jeanne Allen <tebazele@gmail.com>
2022-12-22 11:21:49 -0700

    meal plan model refined

--- supper-setter-capstone/server/models/MealPlan.js
+++ supper-setter-capstone/server/models/MealPlan.js
@@ -3,7 +3,6 @@ const ObjectId = Schema.Types.ObjectId
 
 export const MealPlanSchema = new Schema({
     name: { type: String, default: 'My Meal Plan', maxLength: 30 },
-    days: { type: Array, required: true },
     creatorId: { type: ObjectId, required: true, ref: 'Account' },
 
 }, { timestamps: true, toJSON: { virtuals: true } })
