ae7cb25702c86415e55cbc9b9e0df15b321c7d68
Devin Withoft <teddyfest@gmail.com>
2023-01-05 14:33:51 -0700

    started add planned meals

--- supper-setter-capstone/server/models/Day.js
+++ supper-setter-capstone/server/models/Day.js
@@ -2,16 +2,16 @@ import { Schema } from "mongoose";
 
 const ObjectId = Schema.Types.ObjectId
 export const DaySchema = new Schema({
-    mealPlanId: { type: ObjectId, required: true, ref: 'MealPlan' },
-    breakfastRecipes: { type: Array, required: true, default: [] },
-    lunchRecipes: { type: Array, required: true, default: [] },
-    dinnerRecipes: { type: Array, required: true, default: [] }
+  mealPlanId: { type: ObjectId, required: true, ref: 'MealPlan' },
+  // breakfastRecipes: { type: Array, required: true, default: [] },
+  // lunchRecipes: { type: Array, required: true, default: [] },
+  // dinnerRecipes: { type: Array, required: true, default: [] }
 }, { timestamps: true, toJSON: { virtuals: true } })
 
 
 DaySchema.virtual('mealPlan', {
-    localField: 'mealPlanId',
-    ref: 'MealPlan',
-    foreignField: '_id',
-    justOne: true
+  localField: 'mealPlanId',
+  ref: 'MealPlan',
+  foreignField: '_id',
+  justOne: true
 })
