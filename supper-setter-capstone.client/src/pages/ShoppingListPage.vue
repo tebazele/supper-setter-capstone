cee16698a1b704f2213d43d4e3901a005364277b
Devin Withoft <teddyfest@gmail.com>
2023-01-11 12:03:54 -0700

    Started getPlannedMealsBySingleDay

--- supper-setter-capstone.client/src/pages/ShoppingListPage.vue
+++ supper-setter-capstone.client/src/pages/ShoppingListPage.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="component">
+  <div class="component">
 
 
-    </div>
+  </div>
 </template>
 
 
@@ -12,28 +12,28 @@ import { computed, reactive, onMounted } from 'vue';
 import { useRoute, useRouter } from "vue-router";
 import { logger } from "../utils/Logger";
 export default {
-    setup() {
-        const route = useRoute();
-        onMounted(() => {
-            loadShoppingList()
-        })
+  setup() {
+    const route = useRoute();
+    onMounted(() => {
+      loadShoppingList()
+    })
 
-        async function loadShoppingList() {
-            logger.log(route.query)
-            logger.log(this.$route.query.test)
-            if (route.query.params = day) {
-                logger.log(day)
-            }
+    async function loadShoppingList() {
+      logger.log(route.params)
+      logger.log(this.$route.query.test)
+      if (route.query.params = day) {
+        logger.log(day)
+      }
 
-        }
-
-        return {
-            days: computed(() => AppState.activeDays)
-
-
-
-        }
     }
+
+    return {
+      days: computed(() => AppState.activeDays)
+
+
+
+    }
+  }
 };
 </script>
 
