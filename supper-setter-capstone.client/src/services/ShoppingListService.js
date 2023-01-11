6bfb9ea27705d438cde1d9c8ae0a4308608f44e4
Devin Withoft <teddyfest@gmail.com>
2023-01-11 12:09:22 -0700

    testing shopping list

---
+++ supper-setter-capstone.client/src/services/ShoppingListService.js
@@ -0,0 +1,13 @@
+import { logger } from "../utils/Logger"
+import { plannedMealsService } from "./PlannedMealsService.js"
+
+
+class ShoppingListService {
+
+  async getShoppingListByDayId(dayId) {
+    logger.log(dayId, 'this is id')
+    const day = await plannedMealsService.getPlannedMealsByDayId(dayId)
+    logger.log(day)
+  }
+
+}
