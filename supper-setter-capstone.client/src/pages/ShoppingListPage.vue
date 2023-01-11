a3ec6b8df1498ec1e62c86894daedbcafbe9fc0b
LiamSmith1992 <liamj.smith@hotmail.com>
2023-01-11 11:45:00 -0700

    shopping list stuff

--- supper-setter-capstone.client/src/pages/ShoppingListPage.vue
+++ supper-setter-capstone.client/src/pages/ShoppingListPage.vue
@@ -9,9 +9,30 @@
 <script>
 import { AppState } from '../AppState';
 import { computed, reactive, onMounted } from 'vue';
+import { useRoute, useRouter } from "vue-router";
+import { logger } from "../utils/Logger";
 export default {
     setup() {
-        return {}
+        const route = useRoute();
+        onMounted(() => {
+            loadShoppingList()
+        })
+
+        async function loadShoppingList() {
+            logger.log(route.query)
+            logger.log(this.$route.query.test)
+            if (route.query.params = day) {
+                logger.log(day)
+            }
+
+        }
+
+        return {
+            days: computed(() => AppState.activeDays)
+
+
+
+        }
     }
 };
 </script>
