b470b45be8f60a68aca29833d97bfc5af17614e4
Devin Withoft <teddyfest@gmail.com>
2023-01-12 14:31:19 -0700

    If shop list generated from day, shows day mp name

--- supper-setter-capstone.client/src/pages/ShoppingListPage.vue
+++ supper-setter-capstone.client/src/pages/ShoppingListPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Shopping list for <span v-if="route.query.day"></span></h1>
+    <h2><span v-if="route.query.day">{{ day.name }} of {{ day.mealPlan.name }}</span> shopping list </h2>
 
     <div v-for="i in ingredients" class="d-flex ">
 
@@ -56,6 +56,7 @@ export default {
     return {
       days: computed(() => AppState.activeDays),
       ingredients: computed(() => AppState.shoppingList),
+      day: computed(() => AppState.activeDay),
       route
     }
   }
