75e1f9bed6dfd8fa1945c18222ad305e2c164987
Devin Withoft <teddyfest@gmail.com>
2023-01-12 14:47:46 -0700

    Shoppinglist name shows based on where routed from

--- supper-setter-capstone.client/src/pages/ShoppingListPage.vue
+++ supper-setter-capstone.client/src/pages/ShoppingListPage.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-
-
+    <h2 v-if="activeDay && route.query.day" class="p-1 mb-3">{{ activeDay.name }} of {{ activeDay.mealPlan.name }}
+      shopping
+      list </h2>
+    <h2 v-if="activeMealPlan && route.query.mealplan" class="p-1 mb-3">{{ activeMealPlan.name }} shopping
+      list </h2>
     <div v-for="i in ingredients" class="d-flex ">
 
       <div class="form-check ms-2">
@@ -35,6 +38,7 @@ import { logger } from "../utils/Logger";
 import { shoppingListService } from "../services/ShoppingListService.js";
 import Pop from "../utils/Pop.js";
 import { daysService } from "../services/DaysService.js";
+import { mealPlansService } from "../services/MealPlansService.js";
 export default {
   setup() {
     const route = useRoute();
@@ -49,7 +53,7 @@ export default {
       }
       if (route.query.mealplan) {
         await shoppingListService.getShoppingListByMealPlanId(route.query.mealplan)
-
+        await mealPlansService.getMealPlanById(route.query.mealplan)
       }
     }
 
@@ -57,6 +61,7 @@ export default {
       days: computed(() => AppState.activeDays),
       ingredients: computed(() => AppState.shoppingList),
       activeDay: computed(() => AppState.activeDay),
+      activeMealPlan: computed(() => AppState.activeMealPlan),
       route
     }
   }
