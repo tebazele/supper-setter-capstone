1d2ced42d9c6f130ee9bcd79d32b5c7a3e8b853f
devin withoft <teddyfest@gmail.com>
2023-02-09 10:37:22 -0700

    Changed DbContext for ingrd seperate from recipe

--- supper-setter-capstone/server/db/DbContext.js
+++ supper-setter-capstone/server/db/DbContext.js
@@ -3,9 +3,10 @@ import { AccountSchema } from '../models/Account'
 import { DaySchema } from '../models/Day.js';
 import { MealPlanSchema } from '../models/MealPlan.js';
 import { PlannedMealSchema } from '../models/PlannedMeal.js';
-import { IngredientSchema, RecipeSchema } from '../models/Recipe.js';
+import { RecipeSchema } from '../models/Recipe.js';
 import { ShoppingListSchema } from "../models/ShoppingList.js";
 import { ValueSchema } from '../models/Value'
+import { IngredientSchema } from "../models/Ingredient.js";
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
