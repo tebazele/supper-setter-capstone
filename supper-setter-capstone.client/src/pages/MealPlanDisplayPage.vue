3c8165615b17c0aef95332a56e0a47747575b828
devin withoft <teddyfest@gmail.com>
2023-02-09 15:57:38 -0700

    working on v-if models

--- supper-setter-capstone.client/src/pages/MealPlanDisplayPage.vue
+++ supper-setter-capstone.client/src/pages/MealPlanDisplayPage.vue
@@ -1,30 +1,30 @@
 <template>
-    <div class="container-fluid">
-        <section v-if="displayForm" class="row justify-content-between">
-            <div class="col-8">
-                <!-- STUB write the post function -->
-                <form class="d-flex mt-3" @submit.prevent="createMealPlan">
-                    <input placeholder="Give your meal plan a name" class="w-100" v-model="mealPlanName"
-                        maxlength="50" /><button class="btn btn-primary">Save</button>
-                </form>
-                <!-- NOTE form validation on front end done -->
+  <div class="container-fluid">
+    <section v-if="displayForm" class="row justify-content-between">
+      <div class="col-8">
+        <!-- STUB write the post function -->
+        <form class="d-flex mt-3" @submit.prevent="createMealPlan">
+          <input placeholder="Mealplan Name" class="w-100" v-model="mealPlanName" maxlength="50" required /><button
+            class="btn btn-primary">Create</button>
+        </form>
+        <!-- NOTE form validation on front end done -->
 
-            </div>
-            <div class="col-4 text-end mt-3">
-                <button class="btn btn-info">Add Day</button>
-            </div>
-        </section>
-        <section v-else class="row">
-            <div class="col-12">
-                <h4 class="mt-2">{{ mealPlanName }} <i @click="editMealPlanName" class="mdi mdi-pen"></i></h4>
+      </div>
+      <div class="col-4 text-end mt-3">
+        <button class="btn btn-info">Add Day</button>
+      </div>
+    </section>
+    <section v-else class="row">
+      <div class="col-12">
+        <h4 class="mt-2">{{ mealPlanName }} <i @click="editMealPlanName" class="mdi mdi-pen"></i></h4>
 
-            </div>
-        </section>
+      </div>
+    </section>
 
-        <!-- <MealPlan /> -->
+    <!-- <MealPlan /> -->
 
 
-    </div>
+  </div>
 </template>
 
 
@@ -38,33 +38,33 @@ import { mealPlansService } from '../services/MealPlansService.js'
 import { router } from "../router";
 import { useRouter } from 'vue-router';
 export default {
-    setup() {
-        const router = useRouter()
-        const mealPlanName = ref()
-        const displayForm = ref(true)
+  setup() {
+    const router = useRouter()
+    const mealPlanName = ref()
+    const displayForm = ref(true)
 
 
-        return {
-            mealPlanName,
-            displayForm,
-            async createMealPlan() {
-                try {
+    return {
+      mealPlanName,
+      displayForm,
+      async createMealPlan() {
+        try {
 
-                    // logger.log(mealPlanName.value)
-                    const mealPlan = await mealPlansService.createMealPlan(mealPlanName.value)
-                    logger.log('These are the days on the new plan' + AppState.activeDays)
-                    logger.log('These are the planned meals on the new plan' + AppState.plannedMeals)
-                    displayForm.value = false
-                    Pop.toast('meal plan created')
-                    router.push({ name: 'MealPlanDetails', params: { mealPlanId: mealPlan.id } })
-                } catch (error) {
-                    logger.log(error)
-                    Pop.error(error.message)
-                }
-            }
-        };
-    },
-    components: { MealPlan }
+          // logger.log(mealPlanName.value)
+          const mealPlan = await mealPlansService.createMealPlan(mealPlanName.value)
+          logger.log('These are the days on the new plan' + AppState.activeDays)
+          logger.log('These are the planned meals on the new plan' + AppState.plannedMeals)
+          displayForm.value = false
+          Pop.toast('meal plan created')
+          router.push({ name: 'MealPlanDetails', params: { mealPlanId: mealPlan.id } })
+        } catch (error) {
+          logger.log(error)
+          Pop.error(error.message)
+        }
+      }
+    };
+  },
+  components: { MealPlan }
 };
 </script>
 
